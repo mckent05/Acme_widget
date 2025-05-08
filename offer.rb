dfdeccc75918e2aa79155b92ee62bf7850dd42e8
mckent05 <atemitope95@gmail.com>
2025-05-08 09:45:40 +0100

    Added offer class and method to apply valid offers

---
+++ offer.rb
@@ -0,0 +1,16 @@
+class Offer
+  def initialize(discount_terms)
+    @discount_terms = discount_terms
+  end
+
+  def apply_offer(cart_items)
+    cart_items_discount = 0
+    if @discount_terms[:discount_type] == :buy_one_get_one_half_price
+      product_code = @discount_terms[:product_code]
+      if product_code == "R01"
+        items = cart_items.filter { |cart_item| cart_item.product_code == product_code }
+        cart_items_discount = (items.count / 2 ) * (@discount_terms[:discount_rate] * items[0].price)
+      end
+    end
+  end
+end
